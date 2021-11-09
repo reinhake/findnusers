590460d102b73f0e0963bf423fabbc27210dd029
Keth Reinhardt <reinhake@oregonstate.edu>
2021-11-09 13:41:53 -0800

    first test

--- server.js
+++ server.js
@@ -1,1 +1,24 @@
-First stuffs
+const express = require("express")
+
+const app = express();
+
+
+app.use(express.json());
+app.set('port', process.env.PORT || 8080);
+
+
+app.get('/findUsers', async (reg, res) =>{
+	
+	
+	user = req.query.user
+	
+	locations = req.query.getAll('locations')
+	
+})
+
+
+app.listen(app.get('port'), () => 
+  console.log(`it's alive on https://findnusers-apim.azure-api.net`)
+);
+
+// https://findnusers-apim.azure-api.net/findUsers?user=0.000000,0.000000&locations=1.111111&locations=1.111111
